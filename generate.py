6257a0ff06465eb7b2875be8f13f9d2d91ce35a1
xuruifan <707185547@qq.com>
2023-09-06 13:43:33 +0800

    Create systolic array with modularity

--- generate.py
+++ generate.py
@@ -1,11 +1,28 @@
 import sys
 N = int(sys.argv[1])
 def PE_def(i, j):
-    return "%PE_{}_{}".format(str(i), str(j))
+    return "%PE_{}_{}".format(i, j)
 IR = """module {
   hec.design @systolic_array {
     hec.component @PE(%A_in: f32, %C_in: f32) -> (%A_out: f32, %C_out: f32) {interface="wrapped", style="dummy"} {}
 """
+IR += "    hec.component @PE_line(%A: f32, "
+for i in range(N):
+    IR += "%C_in{}: f32, ".format(i)
+IR = IR[:-2] + ") -> ("
+for i in range(N):
+    IR += "%C_out{}: f32, ".format(i)
+IR = IR[:-2] + ") {interface=\"wrapped\", style=\"handshake\"} {\n"
+for i in range(N):
+    PE = "%PE_{}".format(i)
+    IR += "      {}.A_in, {}.C_in, {}.A_out, {}.C_out = hec.instance \"{}\" @PE: f32, f32, f32, f32\n".format(PE, PE, PE, PE, PE[1:])
+IR += "      hec.graph {\n"
+for i in range(N):
+    IR += "        hec.assign {} = {} : f32 -> f32\n".format("%PE_{}.A_in".format(i), "%PE_{}.A_out".format(i-1) if i>0 else "%A")
+    IR += "        hec.assign {} = {} : f32 -> f32\n".format("%PE_{}.C_in".format(i), "%C_in{}".format(i))
+    IR += "        hec.assign {} = {} : f32 -> f32\n".format("%C_out{}".format(i), "%PE_{}.C_out".format(i))
+IR += "      }\n    }\n"
+
 IR += "    hec.component @main("
 for i in range(N):
     IR += "%A" + str(i) + ": f32, "
@@ -16,18 +33,27 @@ for i in range(N):
     IR += "%C_out" + str(i) + ": f32, "
 IR = IR[:-2] + ") {interface=\"wrapped\", style=\"handshake\"} {\n"
 for i in range(N):
+    PE_line = "%Line_{}".format(i)
+    args = ["A"]
     for j in range(N):
-        PE = PE_def(i, j)
-        IR += "      {}.A_in, {}.C_in, {}.A_out, {}.C_out = hec.instance \"{}\" @PE: f32, f32, f32, f32\n".format(PE, PE, PE, PE, PE[1:])
+        args.append("C_in{}".format(j))
+    for j in range(N):
+        args.append("C_out{}".format(j))
+    ports = ""
+    for arg in args:
+        ports += PE_line + "." + arg + ", "
+    IR += "      {} = hec.instance \"{}\" @PE_line : {}\n".format(ports[:-2], PE_line[1:], ("f32, "*len(args))[:-2])
 IR += "      hec.graph {\n"
 for i in range(N):
+    IR += "        hec.assign {} = {} : f32 -> f32\n".format("%Line_{}.A".format(i), "%A{}".format(i))
+for i in range(N):
+    PE_line = "%Line_{}".format(i)
+    last_line = "%Line_{}".format(i-1)
     for j in range(N):
-        PE = PE_def(i, j)
-        IR += "        hec.assign {} = {} : f32 -> f32\n".format(PE + ".A_in", PE_def(i, j-1) + ".A_out" if j>0 else "%A" + str(i))
-        IR += "        hec.assign {} = {} : f32 -> f32\n".format(PE + ".C_in", PE_def(i-1, j) + ".C_out" if i>0 else "%C_in" + str(j))
+        IR += "        hec.assign {} = {} : f32 -> f32\n".format(PE_line + ".C_in{}".format(j),  last_line + ".C_out{}".format(j) if i>0 else "%C_in" + str(j))
 for j in range(N):
-    PE = PE_def(N-1, j)
-    IR += "        hec.assign {} = {} : f32 -> f32\n".format("%C_out" + str(j), PE + ".C_out")
+    Line = "%Line_{}".format(N-1)
+    IR += "        hec.assign {} = {} : f32 -> f32\n".format("%C_out" + str(j), Line + ".C_out{}".format(j))
 IR += "      }\n    }\n  }\n}\n"
 hec_ir = open("hec.mlir", "w")
 hec_ir.write(IR)
@@ -51,13 +77,13 @@ TEST += """  val A = IO(Flipped(Vec(n, DecoupledIO(UInt(32.W)))))
 """
 
 for i in range(N):
-    TEST += "  connection(A({}), main.var{})\n".format(i, i)
+    TEST += "  connection(A({}), main.var{})\n".format(i, i+2*N+1)
 
 for i in range(N):
-    TEST += "  connection(C_in({}), main.var{})\n".format(i, N+i)
+    TEST += "  connection(C_in({}), main.var{})\n".format(i, N+i+2*N+1)
 
 for i in range(N):
-    TEST += "  connection_inverse(C_out({}), main.var{})\n".format(i, 2*N+i)
+    TEST += "  connection_inverse(C_out({}), main.var{})\n".format(i, 2*N+i+2*N+1)
 TEST += "}\n"
 scala = open("wrapper.scala", "w")
 scala.write(TEST)
